63e5ae46b0f485197f21b2ba95281dee0b8ea9eb
Patryk <patryk.gsrmeen@gmail.com>
2019-05-11 18:32:28 +0200

    Add url scraping

--- src/itaka-scraper.py
+++ src/itaka-scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 
 OFFER_CSS = "offer_column offer_column-second col-sm-7 col-lg-8 clearfix"
 
+
 def getURL(**kwargs):
     settings = {
         'departureDate': '2019-05-11',
@@ -11,11 +12,20 @@ def getURL(**kwargs):
 
     settings.update(kwargs)
     resultURL = ("https://www.itaka.pl/last-minute/?view=offerList&" +
-    "package-type=wczasy&adults=" + str(settings["peopleCount"]) +
-    "&date-from=" + settings["departureDate"] + "&food=allInclusive&" +
-    "dep-region=warszawa-modlin%2Cwarszawa&promo=lastMinute&order=priceAsc&" +
-    "total-price=0&page=1&currency=PLN")
+                 "package-type=wczasy&adults=" + str(settings["peopleCount"]) +
+                 "&date-from=" + settings["departureDate"] +
+                 "&food=allInclusive&dep-region=warszawa-modlin%2Cwarszawa&" +
+                 "promo=lastMinute&order=priceAsc&total-price=0&page=1&" +
+                 "transport=flight&currency=PLN")
     return resultURL
+
+
+def getGeoInfo(pageInfo):
+    geoInfo = {}
+    cutParts = pageInfo.split(" / ")
+    geoInfo["country"] = cutParts[0]
+    geoInfo["city"] = cutParts[-1]
+    return geoInfo
 
 
 if __name__ == '__main__':
@@ -25,6 +35,13 @@ if __name__ == '__main__':
 
     for offer in soup.findAll("div", class_=OFFER_CSS):
         hotelInfo = {}
+        hotelLocalization = offer.find("div", class_="header_geo-labels").text
+        price = offer.find("span",
+                           class_="current-price_value").text.replace(" ", "")
+        hotelInfo.update(getGeoInfo(hotelLocalization))
+        offerUrl = offer.find("header",
+                              class_="offer_header").findAll("a")[0]["href"]
         hotelInfo["hotelName"] = offer.find("h2", class_="header_title").text
-        hotelInfo["local"] = offer.find("div", class_="header_geo-labels").text
+        hotelInfo["pricePerPerson"] = int(price)
+        hotelInfo["offerUrl"] = ("https://www.itaka.pl" + offerUrl)
         print(hotelInfo)
